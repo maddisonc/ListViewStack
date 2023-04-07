8551ba749de1b8e619371d820aded89bd9974fc8
Maddison <mtranchau1@gmail.com>
2023-04-07 00:30:36 -0700

    12:30am: populated default month endings stack, wrote for loop code to randomize first letter of the month and ending (tested by printing to log)

--- app/src/main/java/com/example/listviewstack/MainActivity.java
+++ app/src/main/java/com/example/listviewstack/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.listviewstack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     Deque<String> defaultMonthStack = new ArrayDeque<String>();
     Deque<String> newMonthStack = new ArrayDeque<String>();
 
+    String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+                        "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +37,20 @@ public class MainActivity extends AppCompatActivity {
         defaultMonthStack.push("ovember");
         defaultMonthStack.push("ecember");
 
+        Object[] months = defaultMonthStack.toArray();
+
         // adds 25 "new months"
         for (int i = 0; i < 25; i++)
         {
-            newMonthStack.push("" + defaultMonthStack.peek());
+            // picks a random letter and random month ending for each iteration
+            int ranLetterIndex = (int)(Math.floor(Math.random() * 25));
+            int ranMonthIndex = (int)(Math.floor(Math.random() * 12));
+
+            // pushes a random letter + random month ending
+            newMonthStack.push(letters[ranLetterIndex] + "" + months[ranMonthIndex]);
         } // end for loop for adding new months
 
+        // tests stack, prints to log
+        Log.i("test", "newmonths" + newMonthStack);
     } // end onCreate
 } // end MainActivity
