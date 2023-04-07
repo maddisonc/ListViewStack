f1ad157f41e0f21894ccbbff985733e762aa4d3a
Maddison <mtranchau1@gmail.com>
2023-04-07 00:08:35 -0700

    12:07am: made two stacks - one that contains the endings of months, a new, empty one that will use a for loop to push "new months" - months that are the same as the old ones but with randomized first letters

--- app/src/main/java/com/example/listviewstack/MainActivity.java
+++ app/src/main/java/com/example/listviewstack/MainActivity.java
@@ -9,11 +9,35 @@ import java.util.Deque;
 
 public class MainActivity extends AppCompatActivity {
 
-    Deque<String> stack = new ArrayDeque<String>();
+    // declared
+    Deque<String> defaultMonthStack = new ArrayDeque<String>();
+    Deque<String> newMonthStack = new ArrayDeque<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // populated
+        defaultMonthStack.push("anuary");
+        defaultMonthStack.push("ebruary");
+        defaultMonthStack.push("arch");
+        defaultMonthStack.push("pril");
+        defaultMonthStack.push("ay");
+        defaultMonthStack.push("une");
+        defaultMonthStack.push("uly");
+        defaultMonthStack.push("ugust");
+        defaultMonthStack.push("eptember");
+        defaultMonthStack.push("ctober");
+        defaultMonthStack.push("ovember");
+        defaultMonthStack.push("ecember");
+
+        // adds 25 "new months"
+        for (int i = 0; i < 25; i++)
+        {
+            newMonthStack.push("" + defaultMonthStack.peek());
+        } // end for loop for adding new months
+
+    } // end onCreate
+} // end MainActivity
