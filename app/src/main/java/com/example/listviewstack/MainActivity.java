782990195eff188eb05e3bf5957d70a54443505f
Maddison <mtranchau1@gmail.com>
2023-04-07 00:59:06 -0700

    12:58am: main activity now displays random new months in listView, added spaces before each month for readability, now displaying more months (25 to 75) to show overflow

--- app/src/main/java/com/example/listviewstack/MainActivity.java
+++ app/src/main/java/com/example/listviewstack/MainActivity.java
@@ -4,26 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
-import java.util.Vector;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
-    // declared
+    // declaring stacks, arr, and arrlist
     Deque<String> defaultMonthStack = new ArrayDeque<String>();
     Deque<String> newMonthStack = new ArrayDeque<String>();
 
     String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
                         "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
+    ArrayList<String> newMonthStrings = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // populated
+        // configuring listView
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, R.layout.activity_list_view, R.id.textView, newMonthStrings);
+        listView.setAdapter(arrayAdapter);
+
+        // populated month endings
         defaultMonthStack.push("anuary");
         defaultMonthStack.push("ebruary");
         defaultMonthStack.push("arch");
@@ -37,10 +49,11 @@ public class MainActivity extends AppCompatActivity {
         defaultMonthStack.push("ovember");
         defaultMonthStack.push("ecember");
 
+        // object arr for random month ending
         Object[] months = defaultMonthStack.toArray();
 
-        // adds 25 "new months"
-        for (int i = 0; i < 25; i++)
+        // populating 75 "new months"
+        for (int i = 0; i < 75; i++)
         {
             // picks a random letter and random month ending for each iteration
             int ranLetterIndex = (int)(Math.floor(Math.random() * 25));
@@ -52,5 +65,15 @@ public class MainActivity extends AppCompatActivity {
 
         // tests stack, prints to log
         Log.i("test", "newmonths" + newMonthStack);
+
+        // creates an iterator to go through the stack - adds each value to a string arrList for displaying in listView
+        Iterator value = newMonthStack.iterator();
+        // while there is a next value, add to str arrList
+        while (value.hasNext())
+        {
+            // spaces for readability
+            newMonthStrings.add("     " + value.next());
+        } // end while
+
     } // end onCreate
 } // end MainActivity
