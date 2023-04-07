9b11782396a00a33cf6341512273f7438af67106
Maddison <mtranchau1@gmail.com>
2023-04-07 00:00:16 -0700

    11:59pm: imported deque interface for stack creation, created stack, next steps: push data to stack, display in listView

--- app/src/main/java/com/example/listviewstack/MainActivity.java
+++ app/src/main/java/com/example/listviewstack/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class MainActivity extends AppCompatActivity {
+
+    Deque<String> stack = new ArrayDeque<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
